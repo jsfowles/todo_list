f6523f75e659b3ebe1d6691e132aeb1d472fbc31
Jacob Fowles <js.fowles@gmail.com>
2016-05-24 14:21:55 -0600

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'items/index'
+
+  get 'items/new'
+
+  get 'items/edit'
+
+  get 'items/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
